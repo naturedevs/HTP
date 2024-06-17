42cb23adf35485624bf393d52bc25921a309dc5e
snowflake515 <davidviera0515@gmail.com>
2024-06-18 00:08:32 +0800

    fix header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -45,13 +45,13 @@ export function NavBar({backend}) {
       <div className="top-0 sticky z-50 flex bg-white">
         <div className="flex flex-row w-full lg:justify-between md:justify-center sm:justify-between xs:justify-between sticky top-0 max-w-[1280px] mx-auto px-[15px]">
           <div className="flex h-[99px]">
-            <Link href="/" className={backend?"sm:ml-0 ml-10 transition-all":""}>
+            <Link href="/" className="items-center content-center">
               <Image
                 src="/logo.svg"
                 alt="Kiwi Farms - Premium Cannabis"
-                width={342}
+                width={325}
                 height={30}
-                className="md:max-lg:hidden mt-[34px]"
+                className="md:max-lg:hidden"
               />
             </Link>
           </div>
