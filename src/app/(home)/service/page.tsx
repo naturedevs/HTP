94b2dc8d39e028aef02daac88c1da8076e10f363
Kota <kotaendo118@outlook.com>
2024-07-16 18:41:13 +0900

    upt4

---
+++ src/app/(home)/service/page.tsx
@@ -0,0 +1,5 @@
+export default function ServicePage() {
+	return (
+		<p>This is service page.</p>
+	);
+}
--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,98 +2,101 @@
 import Link from "next/link";
 
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { ExitIcon } from "@radix-ui/react-icons";
 import { useRouter } from "next/navigation";
 import {
-  logout,
-  selectIslogin,
-  selectIsVisitor,
-  selectIsNothing
+	logout,
+	selectIslogin,
+	selectIsVisitor,
+	selectIsNothing
 } from "@/store/features/auth/authSlice";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { Button } from "./ui/button";
 
 export function NavMenu() {
-  const router = useRouter();
-  const isLogin = useAppSelector(selectIslogin); 
-  const dispatch = useAppDispatch();
 
-  const handleLogout = () => {
-    router.push("/");
-    dispatch(logout());
-  }
+	const router = useRouter();
+	const isLogin = useAppSelector(selectIslogin); 
+	const dispatch = useAppDispatch();
 
-  return (
-    <NavigationMenu className="hidden md:flex w-full">
-      <NavigationMenuList className="w-full">
-        <div className="space-x-6 flex">
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink>
-                HOME
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+	const handleLogout = () => {
+		router.push("/");
+		dispatch(logout());
+	}
 
-          <NavigationMenuItem>
-            <Link href="/events" legacyBehavior passHref>
-              <NavigationMenuLink>
-                FIND EVENTS
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+	return (
+		<NavigationMenu className="hidden md:flex w-full">
+			<NavigationMenuList className="w-full">
+				<div className="space-x-6 flex">
+					
+					<NavigationMenuItem>
+						<Link href="/" legacyBehavior passHref>
+							<NavigationMenuLink>
+								HOME
+							</NavigationMenuLink>
+						</Link>
+					</NavigationMenuItem>
 
-          <NavigationMenuItem>
-            <Link href="/media" legacyBehavior passHref>
-              <NavigationMenuLink>
-                MEDIA
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+					<NavigationMenuItem>
+						<Link href="/events" legacyBehavior passHref>
+							<NavigationMenuLink>
+								FIND EVENTS
+							</NavigationMenuLink>
+						</Link>
+					</NavigationMenuItem>
 
-          <NavigationMenuItem>
-            <Link href="/services" legacyBehavior passHref>
-              <NavigationMenuLink>
-                OUR SERVICES
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </div>
+					<NavigationMenuItem>
+						<Link href="/media" legacyBehavior passHref>
+							<NavigationMenuLink>
+								MEDIA
+							</NavigationMenuLink>
+						</Link>
+					</NavigationMenuItem>
 
-        {!isLogin && <>
-          <NavigationMenuItem>
-            <Link href="/signup" legacyBehavior passHref>
-              <button className="bg-black hover:bg-[#009688] text-gray-50 w-[136px] h-[42px] rounded-[21px] ml-4">
-                SIGN UP
-              </button>
-            </Link>
-          </NavigationMenuItem>
+					<NavigationMenuItem>
+						<Link href="/service" legacyBehavior passHref>
+							<NavigationMenuLink>
+								OUR SERVICES
+							</NavigationMenuLink>
+						</Link>
+					</NavigationMenuItem>
+				</div>
 
-          <NavigationMenuItem>
-            <Link href="/login" legacyBehavior passHref>
-              <button className="bg-[#34A853] hover:bg-[#009688] text-gray-50 w-[136px] h-[42px] rounded-[24.5px] ml-2">
-                LOGIN
-              </button>
-            </Link>
-          </NavigationMenuItem>
-        </>}
+				{!isLogin && <>
+					<NavigationMenuItem>
+						<Link href="/signup" legacyBehavior passHref>
+							<button className="bg-black hover:bg-[#009688] text-gray-50 w-[136px] h-[42px] rounded-[21px] ml-4">
+								SIGN UP
+							</button>
+						</Link>
+					</NavigationMenuItem>
 
-        {isLogin &&
-          <NavigationMenuItem>
-            <div className="flex h-16 items-center" onClick={handleLogout}>
-              <div className="flex items-center justify-center text-xl py-2 px-4 rounded-sm bg-slate-50 hover:bg-slate-200 cursor-pointer">
-                <span ><ExitIcon width={30} height={30}/></span>
-              </div>
-            </div>
-          </NavigationMenuItem>
-        }
+					<NavigationMenuItem>
+						<Link href="/login" legacyBehavior passHref>
+							<button className="bg-[#34A853] hover:bg-[#009688] text-gray-50 w-[136px] h-[42px] rounded-[24.5px] ml-2">
+								LOGIN
+							</button>
+						</Link>
+					</NavigationMenuItem>
+				</>}
 
-      </NavigationMenuList>
-    </NavigationMenu>
-  );
+				{isLogin &&
+					<NavigationMenuItem>
+						<div className="flex h-16 items-center" onClick={handleLogout}>
+							<div className="flex items-center justify-center text-xl py-2 px-4 rounded-sm bg-slate-50 hover:bg-slate-200 cursor-pointer">
+								<span ><ExitIcon width={30} height={30}/></span>
+							</div>
+						</div>
+					</NavigationMenuItem>
+				}
+
+			</NavigationMenuList>
+		</NavigationMenu>
+	);
+
 }
