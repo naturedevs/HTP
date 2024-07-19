d8e91f158306fe0972ac4822f1374eea55536a9b
Kota <kotaendo118@outlook.com>
2024-07-19 11:06:41 +0900

    upt-719-2

--- src/components/home/mediaCategory.tsx
+++ src/components/home/mediaCategory.tsx
@@ -1,11 +1,11 @@
-import { url } from "inspector";
 import Image from "next/image";
+import PropTypes from 'prop-types';
 
-export default function MediaCategory() {
+export default function MediaCategory({title}) {
 
    return(
       <div className="w-full flex">
-         <p className="md:font-[400] md:text-[17px] md:leading-[18.5px] font-[400] text-[14px] leading-[15.23px] text-[#1F1E1E] mt-1 line-clamp-2 flex-1">Lainey Wilson: Country's Cool Again Tour</p>
+         <p className="md:font-[400] md:text-[17px] md:leading-[18.5px] font-[400] text-[14px] leading-[15.23px] text-[#1F1E1E] mt-1 line-clamp-2 flex-1">{title}</p>
          <Image
             src="/media_arrow.svg"
             alt=""
@@ -16,3 +16,11 @@ export default function MediaCategory() {
       </div>
    );
 }
+
+MediaCategory.propTypes = {
+   title: PropTypes.string,
+};
+ 
+MediaCategory.defaultProps = {
+   title: "Lainey Wilson: Country's Cool Again Tour",
+};
--- src/components/home/mediaItem.tsx
+++ src/components/home/mediaItem.tsx
@@ -1,27 +1,25 @@
 import { url } from "inspector";
 import Image from "next/image";
+import PropTypes from 'prop-types';
 
-export default function MediaItem() {
+export default function MediaItem({thumbnail, creator, creator_avatar, created_at, title, description}) {
 
    return(
       <div className="w-full">
-         <div className="w-full h-[229px]" style={{ backgroundImage: "url('/homeBg4_1.svg')" }}></div>
+         <div className="w-full h-[229px]" style={{ backgroundImage: `url(${thumbnail})` }}></div>
          <div className="w-full flex items-center mt-1">
             <Image
-               src="/avatar1.svg"
+               src={creator_avatar}
                alt=""
                width={34}
                height={34}
             />
-            <p className="text-[#777777] text-[14px] leading-[14px] ml-2">Jenny kiaa</p>
+            <p className="text-[#777777] text-[14px] leading-[14px] ml-2">{creator}</p>
             <p className="text-[#777777] text-[14px] leading-[14px] ml-2">|</p>
-            <p className="text-[#777777] text-[14px] leading-[14px] ml-2">02 April 2022</p>  
+            <p className="text-[#777777] text-[14px] leading-[14px] ml-2">{created_at}</p>
          </div>
-
-         <p className="md:font-[500] md:text-[21px] md:leading-[27px] font-[700] text-[22px] leading-[27px] text-black mt-1">Grad your spot fast before all the seats fill up.</p>
-
-         <p className="md:font-[400] md:text-[16px] md:leading-[22px] font-[400] text-[14px] leading-[18px] text-[#010101] mt-1 line-clamp-2">Grad your spot fast before all the seats fill up, don’t miss it.....</p>
-
+         <p className="md:font-[500] md:text-[21px] md:leading-[27px] font-[700] text-[22px] leading-[27px] text-black mt-1">{title}</p>
+         <p className="md:font-[400] md:text-[16px] md:leading-[22px] font-[400] text-[14px] leading-[18px] text-[#010101] mt-1 line-clamp-2">{description}</p>
          <div className="flex justify-start mt-3">
             <button className="md:w-[167px] md:h-[51px] w-[116px] h-[39px] rounded-[25.5px] bg-primaryColor md:font-[600] md:text-[17px] md:leading-[18.5px] text-[#FFFFFF]">
                READ MORE
@@ -30,3 +28,21 @@ export default function MediaItem() {
       </div>
    );
 }
+
+MediaItem.propTypes = {
+   thumbnail: PropTypes.string,
+   creator: PropTypes.string,
+   creator_avatar: PropTypes.string,
+   created_at: PropTypes.string,
+   title: PropTypes.string,
+   description: PropTypes.string
+};
+ 
+MediaItem.defaultProps = {
+   thumbnail: '/homeBg4_1.svg',
+   creator: 'Jenny kiaa',
+   creator_avatar: '/avatar1.svg',
+   created_at: '02 April 2022',
+   title: 'Grad your spot fast before all the seats fill up.',
+   description: 'Grad your spot fast before all the seats fill up, Grad your spot fast before all the seats fill up',
+};
