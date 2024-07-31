7a6d2f4d785e7a5c38173869f0e516c9ca247c83
ican0220 <icandev0220@gmail.com>
2024-07-31 23:06:39 +0900

    event create api completed.

--- src/app/api/events/createEvent/route.ts
+++ src/app/api/events/createEvent/route.ts
@@ -1,21 +1,31 @@
 import { NextResponse } from 'next/server';
 import supabase from '@/supabase/supabaseClient';
-import multer from 'multer';
-const  upload = multer({ dest: 'uploads/' });
-export async function POST(request: Request, params: { action: string }) {
-    upload(request)
-    const formData = await request.formData();
 
+const saveFile = async (formData, key) => {
+    const file = formData.get(key) as File;
+    if (file instanceof File) {
+        const fileData = Buffer.from(await file?.arrayBuffer());
+        const fileName = `${Date.now()}${file.name.substring(file.name.lastIndexOf('.'))}`; // Change the filename if needed
+        const { error } = await supabase.storage.from('upload_img').upload(fileName, fileData)
+        console.log(error)
+        const { data } = supabase.storage.from('upload_img').getPublicUrl(fileName)
+        return data.publicUrl;
+    }
+    return "";
+}
+
+export async function POST(request: Request, params: { action: string }) {
+    const formData = await request.formData();
+    const imgUrl = await saveFile(formData, "imgFile");
     let main_info = formData.get('main_info');
     let ticketList = formData.get('ticketList');
-    let imgFile = formData.get('imgFile');
-    
+
     const {
         eventName, eventType, musicType, djName, venueName, dressCode, venueType, venueAddress, ageRestrictions, coverCharge, eventDate, eventStart, eventEnd
     } = JSON.parse(main_info as string);
     ticketList = JSON.parse(ticketList as string);
-    
-    const {error} = await supabase
+
+    const { error } = await supabase
         .from('events')
         .insert({
             event_name: eventName,
@@ -26,14 +36,15 @@ export async function POST(request: Request, params: { action: string }) {
             dress_code: dressCode,
             venue_type: venueType,
             venue_address: venueAddress,
-            age_regstrictions: ageRestrictions,
+            age_restrictions: ageRestrictions,
             cover_charge: coverCharge,
             event_date: eventDate,
             event_start_time: eventStart,
-            event_end_time: eventEnd
+            event_end_time: eventEnd,
+            img_file: imgUrl
         });
 
-    if( error ) return NextResponse.json(JSON.stringify(error), {status: 500})
+    if (error) NextResponse.json(JSON.stringify(error), { status: 400 })
     return NextResponse.json('ok', { status: 200 });
 }
 
