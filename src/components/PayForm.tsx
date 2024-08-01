2e310ed5db662112f44bfb7d135c7f8658155896
ican0220 <icandev0220@gmail.com>
2024-08-02 02:43:33 +0900

    com

--- src/components/PayForm.tsx
+++ src/components/PayForm.tsx
@@ -176,7 +176,7 @@ export default function TestPage({event_id}) {
 
 						<div className="flex-1">
 							<div className="bg-white m-5 p-5 space-y-3">
-								<div className="w-full h-[250px] bg-cover bg-center" style={{ backgroundImage: "url('/test.png')" }}></div>
+								<div className="w-full h-[250px] bg-cover bg-center" style={{ backgroundImage:event.img_file }}></div>
 								<p className="md:font-[500] md:text-[24px] md:leading-[24px] font-[500] text-[24px] leading-[24px] text-black line-clamp-2">{event.name}</p>
 								<p className="text-[#777777] text-[14px] leading-[14px]">{month[parseInt(moment(event.date).format('MM'))]} {moment(event.date).format('DD')} {day[(new Date(event.date)).getDay()]} {' - ' + moment(event.date).format('hh:mm A')}</p>
 								<p className="text-[#777777] text-[14px] leading-[14px]">BankPlus Amphitheater at Snowden Grove, Southaven, MS</p>
